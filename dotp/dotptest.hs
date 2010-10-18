8b21a150e638acdadb49ac132f9aae7651f52eb9
Siddharth Agarwal <sid.bugzilla@gmail.com>
2010-10-19 02:07:03 +0530

    Finish off impl

--- dotp/dotptest.hs
+++ dotp/dotptest.hs
@@ -1,7 +1,7 @@
 {-# OPTIONS -fdph-par -Odph #-}
 {-# LANGUAGE PArr, ParallelListComp, CPP #-}
 
-#ifdef PRIM
+#ifdef UNLIFTED
 import DotPPrim
 import Data.Array.Parallel.Unlifted as U
 #else
@@ -10,4 +10,13 @@ import DotPVect
 
 main = print $ dotp (makearr a1 a2) (makearr b1 b2)
   where
-    a1 = dotp 
+    a1 = dotp (makearr 1 20) (makearr 21 40)
+    a2 = dotp (makearr 41 60) (makearr 61 80)
+    b1 = dotp (makearr 81 100) (makearr 101 120)
+    b2 = dotp (makearr 121 140) (makearr 141 160)
+
+#ifdef UNLIFTED
+makearr n m = U.enumFromTo n m
+#else
+makearr n m = [:n..m:]
+#endif
