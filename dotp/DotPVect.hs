e52d040cabac055828a198ee976bf1719a910e84
Siddharth Agarwal <sid.bugzilla@gmail.com>
2010-10-19 19:33:28 +0530

    Attempt to vectorise multilevel dotp. This doesn't work

--- dotp/DotPVect.hs
+++ dotp/DotPVect.hs
@@ -2,7 +2,20 @@
 {-# OPTIONS -fvectorise #-}
 module DotPVect where
 import Data.Array.Parallel.Prelude
+import Data.Array.Parallel.PArray as P
 import Data.Array.Parallel.Prelude.Int as I
 
-dotp :: [:Int:] -> [:Int:] -> Int
-dotp v w = I.sumP [: (I.*) x y | x <- v, y <- w :]
+dotp_internal :: [:Int:] -> [:Int:] -> Int
+dotp_internal v w = I.sumP [: (I.*) x y | x <- v, y <- w :]
+
+dotp :: PArray Int -> PArray Int -> Int
+{-# NOINLINE dotp #-}
+dotp v w = dotp_internal (fromPArrayP v) (fromPArrayP w)
+
+{- Given a number of integers, constructs a dot product out of them. -}
+
+dotp_2level start1 start2 start3 start4 len =
+  dotp_internal [:a1..a2:] [:a1..a2:]
+  where
+    a1 = dotp_internal [:start1..((Prelude.+) start1 len):] [:start2..((Prelude.+) start2 len):]
+    a2 = dotp_internal [:start3..((Prelude.+) start3 len):] [:start4..((Prelude.+) start4 len):]
--- dotp/dotptest.hs
+++ dotp/dotptest.hs
@@ -2,20 +2,14 @@
 
 #ifdef UNLIFTED
 import DotPPrim
-import Data.Array.Parallel.Unlifted as U
+import Data.Array.Parallel.Unlifted as P
 #else
 import DotPVect
+import Data.Array.Parallel.PArray as P
 #endif
 
-main = print $ dotp (makearr a1 a2) (makearr b1 b2)
+main = print $ dotp (makearr a1 (a1 + 40)) (makearr a1 (a1 + 40))
   where
     a1 = dotp (makearr 1 20) (makearr 21 40)
-    a2 = dotp (makearr 41 60) (makearr 61 80)
-    b1 = dotp (makearr 81 100) (makearr 101 120)
-    b2 = dotp (makearr 121 140) (makearr 141 160)
 
-#ifdef UNLIFTED
-makearr n m = U.enumFromTo n m
-#else
-makearr n m = [:n..m:]
-#endif
+makearr n m = P.enumFromTo n m
